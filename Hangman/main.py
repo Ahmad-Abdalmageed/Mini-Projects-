868b4254cb3f6c7a430861403e7d846c66ff3cec
Ahmad <ahmedmageed19@gmail.com>
2019-07-12 19:34:07 +0200

    Hangman---> chosen word and printing game structure

--- Hangman/main.py
+++ Hangman/main.py
@@ -8,24 +8,113 @@
 #
 # Imports
 from pandas import read_csv as read
-from numpy import array as array
 from numpy.random import choice as np_choice
 # We need to import the words text file
 
 file = read('words.txt')
-words = array(file.iloc[:, 0])
+words = [word for word in file.iloc[:, 0] if len(word) >= 4]
+hangmanPics = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
 
 # Used Functions
-def getWord(wordList):
-    choice = np_choice(wordList)
-    return choice
+def get_word(word_list):
+    choice = np_choice(word_list)
+    return choice, len(choice)
 
+
+def game_start():
+    print("Welcome To HANGMAN 0.1", "You`re given only 5 wrong guesses", sep='\n')
+    print("Otherwise, You LOSE")
+    print(hangmanPics[0])
+
+
+def game_processes(size, word):
+    hidden_word = [" _ " for point in range(size)]
+
+    print(''.join(hidden_word))
+    hang = 1
+
+    while hang != 6:
+        guess = input("Enter a guess :")
+        if guess in word:
+            pos = word.find(guess)
+            hidden_word[pos] = guess
+            print('\n'+hangmanPics[hang-1])
+            print(''.join(hidden_word))
+
+        elif guess not in word:
+            print(hangmanPics[hang])
+            hang += 1
+    else:
+        print(hangmanPics[6]+'\n')
+        print("GAME OVER")
 
 
 if __name__ == '__main__':
-    print("Welcome To HANGMAN 0.1", "You`re given only 5 trials to guess the right word",sep= '\n')
-    print("\n")
-    print("Otherwise", "You LOSE", sep='\n')
+    game_start()
+    chosen_word, length = get_word(words)
+    print(chosen_word)
+    game_processes(length, chosen_word)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
