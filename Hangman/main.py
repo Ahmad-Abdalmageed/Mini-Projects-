8f478c6690ce788b6e3f96344d583809aa351315
Ahmad <ahmedmageed19@gmail.com>
2019-07-14 16:51:07 +0200

    Edited printout on winning situation

--- Hangman/main.py
+++ Hangman/main.py
@@ -13,6 +13,8 @@ from numpy.random import choice as np_choice
 
 file = read('words.txt')
 words = [word for word in file.iloc[:, 0] if len(word) >= 4]
+
+# Hangman Pic-Art
 hangmanPics = ['''
   +---+
   |   |
@@ -67,43 +69,78 @@ hangmanPics = ['''
 
 # Used Functions
 def get_word(word_list):
+    """
+     Choosing a random word from a given list
+    """
     choice = np_choice(word_list)
     return choice, len(choice)
 
 
 def game_start():
+    """
+    Prints the first lines of the structure of the game
+    """
     print("Welcome To HANGMAN 0.1", "You`re given only 5 wrong guesses", sep='\n')
     print("Otherwise, You LOSE")
     print(hangmanPics[0])
 
 
-def game_processes(size, word):
-    hidden_word = [" _ " for point in range(size)]
+def find(word, character):
+    """
+    Finds the occurrences of a certain character in a certain word
+    and returns a list of all the occurrences`s indices
+    """
+    occurrences = [position for position, char in enumerate(word) if char == character]
+    return occurrences
+
+
+# Add A function to replace the guess with an '_'
+def game_processes(word, size):
+    """
+    The main game function which takes guesses and count the user
+    attempts, prints the hangman pic-art and congratulates the
+    user for finishing the game
+    """
+    # Hide the chosen word then print it
+    hidden_word = [' _ ' for item in range(size)]
 
     print(''.join(hidden_word))
     hang = 1
 
     while hang != 6:
         guess = input("Enter a guess :")
+
+        # Winning situation
         if guess in word:
-            pos = word.find(guess)
-            hidden_word[pos] = guess
-            print('\n'+hangmanPics[hang-1])
+            positions = find(word, guess)
+            for position in positions:
+                hidden_word[position] = guess
+                    
+            print('\n' + hangmanPics[hang-1])
             print(''.join(hidden_word))
 
+        # Losing situation
         elif guess not in word:
             print(hangmanPics[hang])
+            print(''.join(hidden_word))
             hang += 1
     else:
-        print(hangmanPics[6]+'\n')
-        print("GAME OVER")
+        if ''.join(hidden_word) == word:
+            print("YOU WON")
+
+        else:
+            print(hangmanPics[6] + '\n')
+            print("GAME OVER")
 
 
 if __name__ == '__main__':
+
     game_start()
     chosen_word, length = get_word(words)
+
     print(chosen_word)
-    game_processes(length, chosen_word)
+    game_processes(chosen_word, length)
+
 
 
 
